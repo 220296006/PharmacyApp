ae2515aa60a29545780e8dd0cf22113196d5d314
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-08-17 11:59:35 +0200

    Added navigation page and use component

---
+++ Front-end/pharmacyapp/src/app/components/user/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+
+
+
+
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
