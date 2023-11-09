07d83b9bb512d68005d23b8b28df2a908b53b17d
Thabiso Matsaba <Thabiso.Matsaba@capaciti.org.za>
2023-11-09 15:04:33 +0200

    created home page with slide show

--- Front-end/src/app/components/home/home.component.ts
+++ Front-end/src/app/components/home/home.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import Typed from 'typed.js';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  private currentSlideIndex = 0;
+  private slideshowInterval: any;
 
   ngOnInit(): void {
+    // Initialize Typed.js
+    this.initTyped();
+
+    // this.showNextSlide();
+
+    // Start the slideshow
+    this.startSlideshow();
+  }
+
+  private initTyped(): void {
     const typedElement = document.querySelector('.typed') as HTMLElement | null;
 
     if (typedElement) {
-      const typedStringsAttribute = typedElement.getAttribute('data-typed-items') || '';
+      const typedStringsAttribute =
+        typedElement.getAttribute('data-typed-items') || '';
       const typedStrings = typedStringsAttribute.split(',');
 
       new Typed('.typed', {
@@ -25,5 +36,32 @@ export class HomeComponent implements OnInit {
         loop: true,
       });
     }
+  }
+
+  private startSlideshow(): void {
+    // Set the interval to switch slides every 4 seconds
+    this.slideshowInterval = setInterval(() => {
+      this.showNextSlide();
+    }, 4000);
+  }
+  private showNextSlide(): void {
+    // Get all slides
+    const slides = document.querySelectorAll('.slide');
+
+    // Hide all slides
+    slides.forEach((slide) => {
+      slide.classList.remove('slide-in');
+    });
+
+    // Increment the index
+    this.currentSlideIndex++;
+
+    // If reached the end, go back to the first slide
+    if (this.currentSlideIndex >= slides.length) {
+      this.currentSlideIndex = 0;
+    }
+
+    // Show the next slide
+    slides[this.currentSlideIndex].classList.add('slide-in');
   }
 }
