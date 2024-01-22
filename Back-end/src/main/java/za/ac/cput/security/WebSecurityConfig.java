aeae4de0d4831e369c52116757a36289a1eea1ad
Thabiso Matsaba <220296006@mycput.ac.za>
2024-01-22 19:14:04 +0200

    Implemented web securityconfig, jwtcongfig, jwtutil

--- Back-end/src/main/java/za/ac/cput/security/WebSecurityConfig.java
+++ Back-end/src/main/java/za/ac/cput/security/WebSecurityConfig.java
@@ -2,14 +2,19 @@ package za.ac.cput.security;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 /**
@@ -18,30 +23,50 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
  * @Date : 2024/01/21
  * @Time : 16:01
  **/
+
 @AllArgsConstructor
 @EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .cors().and() // Enable CORS
-                .csrf().disable() // Disable CSRF for simplicity (you may enable it based on your requirements)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll() // Allow pre-flight requests
-                .anyRequest().permitAll(); // Allow all other requests
-
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers(POST, "user/create/**").permitAll()
-//                .antMatchers("/**").authenticated()
-//                .anyRequest().hasAnyRole("ROLE_USER", "ROLE_ADMIN", "ROLE_SYSADMIN", "ROLE_MANAGER")
-//                .and()
-//                .httpBasic(Customizer.withDefaults())
-//                .sessionManagement().sessionCreationPolicy(STATELESS);
+                //Public Access
+                .antMatchers(POST, "/user/register", "/prescription/create", "/medication/create", "/invoice/create", "/inventory/create", "/customer/create").permitAll()
+                .antMatchers(GET, "/user/all", "/user/read/**", "/prescription/all", "/prescription/read/**", "/medication/all", "/medication/read/**", "/invoice/count",
+                        "/invoice/total-billed-amount", "/invoice/all", "/invoice/read/**", "/inventory/medications", "/inventory/all", "/inventory/read/**", "/customer/count", "/customer/all", "/customer/read/**").permitAll()
+                //ADMIN, MANAGER, SYSADMIN Access
+                .antMatchers(PUT, "/prescription/update", "/medication/update", "/invoice/update", "/inventory/update", "/customer/update")
+                .hasAnyRole("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_SYSADMIN")
+                .antMatchers(DELETE, "/prescription/delete/**", "/medication/delete/**", "/invoice/delete/**", "/inventory/delete/**",
+                        "/customer/delete/**").hasAnyRole("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_SYSADMIN")
+                .antMatchers("/**").authenticated()
+                .anyRequest().hasAnyRole("ROLE_USER", "ROLE_ADMIN", "ROLE_SYSADMIN", "ROLE_MANAGER")
+                .and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(authenticationProvider())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
