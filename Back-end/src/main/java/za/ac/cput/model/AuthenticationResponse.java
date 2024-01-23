bf8d9a78b8e835c29fabf6b732eb51b65f034cd4
Thabiso Matsaba <220296006@mycput.ac.za>
2024-01-24 00:15:38 +0200

    Spring security wokrs

---
+++ Back-end/src/main/java/za/ac/cput/model/AuthenticationResponse.java
@@ -0,0 +1,8 @@
+package za.ac.cput.model;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/01/23
+@Time : 22:40
+**/
+public class AuthenticationResponse {
+}
---
+++ Back-end/src/main/java/za/ac/cput/service/implementation/AuthenticationService.java
@@ -0,0 +1,8 @@
+package za.ac.cput.service.implementation;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/01/23
+@Time : 22:34
+**/
+public class AuthenticationService {
+}
