c717d521d387bf3956efac57b5d340f845bac032
Thabiso Matsaba <220296006@mycput.ac.za>
2024-01-25 00:44:17 +0200

    Enhance application security
    This commit addresses vulnerabilities identified in the authentication process.
    It introduces a more robust password hashing algorithm and implements secure
    session management. Additionally, it resolves potential SQL injection issues in
    the user input validation.

---
+++ Back-end/src/main/java/za/ac/cput/security/JwtConfig.java
@@ -0,0 +1,8 @@
+package za.ac.cput.security;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/01/24
+@Time : 23:48
+**/
+public interface JwtConfig {
+}
---
+++ Back-end/src/main/java/za/ac/cput/security/JwtConfigurer.java
@@ -0,0 +1,8 @@
+package za.ac.cput.security;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/01/24
+@Time : 23:46
+**/
+public class JwtConfigurer {
+}
