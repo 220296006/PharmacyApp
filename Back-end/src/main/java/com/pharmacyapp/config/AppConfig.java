676bb059142055647805599104acd3071e646eeb
Thabiso Matsaba <220296006@mycput.ac.za>
2024-06-30 18:22:30 +0200

    fix(mysql): enforce case insensitivity for table names in Docker
    - Added `command: --lower_case_table_names=1` to MySQL service in `docker-compose.yml`
    - Ensured MySQL tables are created and accessed case insensitively in Docker container
    - This resolves SQL errors due to case sensitivity mismatch between local and Docker environments
    
    Issue: Table names were case-sensitive in Docker, causing errors
    Solution: Enforced case insensitivity to match local development behavior

---
+++ Back-end/src/main/java/com/pharmacyapp/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.pharmacyapp.config;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/06/30
+@Time : 16:26
+**/
+public class AppConfig {
+}
---
+++ Back-end/src/main/java/com/pharmacyapp/config/DotenvConfig.java
@@ -0,0 +1,8 @@
+package com.pharmacyapp.config;/**
+@author : Thabiso Matsaba
+@Project : Back-end
+@Date : 2024/06/30
+@Time : 11:59
+**/
+public class DotenvConfig {
+}
